00447208fcaee9e13fc2506c79b5a7c43fa9011d
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-08 19:58:57 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,9 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit 
+    @article = Article.find(params[:id])
   end 
   
   def update 
+    @article = Article.find(params[:id]).update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
   end 
   
 end
